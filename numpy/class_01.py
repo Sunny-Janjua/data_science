94ec5979a9bd29519e7e6d89ca0f49a8ada01299
Sunny Janjua <hussnainmulazam@gmail.com>
2025-04-12 01:27:39 +0500

    third commit

--- numpy/class_01.py
+++ numpy/class_01.py
@@ -1,9 +1,23 @@
-# import numpy as np
-# import pandas as pd
+import numpy as np
 
-# # 1.1.1 Creating a 1D array
-# a = np.array([1, 2, 3, 4, 5])
-# print("1D array:", a)
+# 1.1.1 Creating a 1D array
+a = np.array([1, 2, 3, 4, 5])
+print("1D array:", a)
+
+# 1.1.2 Creating a 2D array
+b = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D array:", b)
+# 1.1.3 Creating a 3D array
+c = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print("3D array:", c)
+# 1.1.4 Creating a 4D array
+d = np.array([[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]])
+print("4D array:", d)
+# 1.1.5 Creating a 5D array
+e = np.array([[[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]]])
+print("5D array:", e)
 
 
-print("hello world")
+# 1.2.1 Creating an array of zeros
+f = np.zeros((2, 3))
+print("Array of zeros:", f)
