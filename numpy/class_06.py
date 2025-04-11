0a8bd229388a0ded70c13b7533b57c1a72129231
Sunny Janjua <hussnainmulazam@gmail.com>
2025-04-12 02:41:20 +0500

    math function

--- numpy/class_06.py
+++ numpy/class_06.py
@@ -1,3 +1,17 @@
 import numpy as np
 
 print("Use mathamatical operation!")
+
+array1=np.array([1,2,3])
+array2=np.array([1,2,3])
+
+print(np.add(array1,array2))
+print(np.subtract(array1,array2))
+print(np.multiply(array1,array2))
+print(np.divide(array1,array2))
+print(np.mod(array1,array2))
+
+print(np.sqrt(array1))
+print(np.exp(array1))
+print(np.power(array1,array2))
+print(np.abs(array1))
